0a0de282d5860f14c1e4596715d684e68c8f43d3
aachenheimer <snake2eyes7@gmail.com>
2025-01-15 13:52:00 -0500

    Add files via upload

--- communicator.py
+++ communicator.py
@@ -5,9 +5,28 @@ import json
 
 selector = selectors.DefaultSelector()
 
-PORT = 7999
+PORT = 7999 #change in config file
+NUMROOMS = 5 #change later obv i forget how many rooms there are / read from json file
+DEFAULTIP = '127.0.0.1' #set later to W003 ip / whatever is in the config file
 
-numOfRooms = 5 #change later obv i forget how many rooms there are / read from json file
+#store in a dictionary why not
+#makes set value function better n easier
+
+#TODO
+#send messages ability
+#change config file ability
+#update (make a command called update) W003 what your committee name and room number is
+#share a master file from W003
+#make the W003 program lol...
+#make input nonblocking
+
+def setValues():
+	configFile = open("config.txt", "r")
+	#for currentLine
+
+	configFile.close()
+
+
 
 def parseInput(rawString):
 	parsedText = []
@@ -43,6 +62,7 @@ def help(input):
 	if len(input) == 1:
 		print("\nsendmsg : sends a message, by default to W003")
 		print("config : opens config menu")
+		print("exit : exits the program")
 		print("type \'help\' followed by the name of the command you want more information on\n")
 	else:
 		if input[1].upper() == "SENDMSG":
@@ -51,25 +71,37 @@ def help(input):
 			
 		elif input[1].upper() == "CONFIG":
 			print("\nOpens the config menu")
-			print("Used to change port, ip, name, and room configuation\n")
+			print("Used to change port, default send ip, name, and room configuation\n")
 
-def sendmsg(msg):
+		elif input[1].upper() == "EXIT":
+			print("\nExits the program without throwing an error\n")
+
+def sendmsg(msg, ip=DEFAULTIP):
 	#doesnt need to be registered under selectors because its very briefly established
 	#also this can be blocking b/c we can wait to send the message
 	sendSocketfd = socket(AF_INET, SOCK_STREAM)
+	sendSocketfd.connect((ip, PORT))
 
 def acceptSocket(receiveSocket):
 	#also doesnt need to be registered under selectors b/c again, very briefly established
 	incomingSocketfd, addr = receiveSocket.accept()
 	incomingMsg = incomingSocketfd.recv(1024)
-	print("\nReceived connection from %s\n" %addr)
+	print("\nReceived connection from %s" %addr[0])
+	print("Received msg : %s\n\a" %incomingMsg.decode())
+	incomingSocketfd.close()
 
 receiveSocketfd = socket(AF_INET, SOCK_STREAM) #FOR RECEIVING MESSAGES FROM OTHER ROOMS
 receiveSocketfd.setblocking(False)
-receiveSocketfd.listen(numOfRooms)
+receiveSocketfd.bind(('127.0.0.1', PORT))
+receiveSocketfd.listen(NUMROOMS)
 selector.register(receiveSocketfd, selectors.EVENT_READ, acceptSocket)
 
 while True:
+	events = selector.select(0)
+	for key in events:
+		callback = key[0].data
+		callback(receiveSocketfd)
+
 	choice = input("Type \'help\' for more options\n>")
 
 	#print("TESTING %s" %choice)
@@ -78,10 +110,14 @@ while True:
 	#print("TESTING : ", parsedInput)
 
 	if parsedInput[0].upper() == "HELP":
-		os.system('cls')
+	#	os.system('cls')
 		help(parsedInput)
 
 	if parsedInput[0].upper() == "EXIT":
-		os.system('cls')
+	#	os.system('cls')
+		print("\nCredit : Andrew DeRose\nThanks!\n")
 		exit()
+
+	else:
+		print("\nUnrecognized command : \"%s\"\n" %choice)
 		
